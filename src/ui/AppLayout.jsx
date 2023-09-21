e1c3e20420a038ff117779635e306b87cb16f2f6
agpavlik <agpavlik@gmail.com>
2023-09-21 14:48:44 -0400

    Add AppLayout as a parent component. Add Header.

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
+
+// Outlet is used within the parent route element to indicate where a child route element should be rendered.
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Pizzolino Co.</Link>
+      <p>User</p>
+    </header>
+  );
+}
+
+export default Header;
