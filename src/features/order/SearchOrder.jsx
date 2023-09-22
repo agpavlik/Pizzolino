9c2ac8514114b376f0570612b53435666f3b4237
agpavlik <agpavlik@gmail.com>
2023-09-21 23:02:47 -0400

    Add SearchOrder component

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          placeholder="Search order #"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        ></input>
+      </div>
+    </form>
+  );
+}
+
+export default SearchOrder;
