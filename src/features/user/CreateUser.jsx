6ae265975670b15b0ac2dd7c9785255d65005b07
agpavlik <agpavlik@gmail.com>
2023-09-24 22:04:34 -0400

    Add style (responsive design)

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -9,13 +9,16 @@ function CreateUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="w-72"
       />
 
       {username !== '' && (
