a4dda8314eff88c834ae202df95be836a3b3cd5f
agpavlik <agpavlik@gmail.com>
2023-09-25 22:53:31 -0400

    Change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: { sans: 'Roboto Mono, monospace' },
+
+    extend: {
+      height: { screen: '100dvh' },
+    },
   },
   plugins: [],
 };
