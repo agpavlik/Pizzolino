de6abdd372e4a7dadce8555380bf19af237a6fe0
agpavlik <agpavlik@gmail.com>
2023-10-30 22:10:21 -0400

    Install Redux. Start modeling the User

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,23 +1,41 @@
-function getPosition() {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-}
+// function getPosition() {
+//   return new Promise(function (resolve, reject) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// }
 
-// eslint-disable-next-line no-unused-vars
-async function fetchAddress() {
-  // 1) We get the user's geolocation position
-  const positionObj = await getPosition();
-  const position = {
-    latitude: positionObj.coords.latitude,
-    longitude: positionObj.coords.longitude,
-  };
+// // eslint-disable-next-line no-unused-vars
+// async function fetchAddress() {
+//   // 1) We get the user's geolocation position
+//   const positionObj = await getPosition();
+//   const position = {
+//     latitude: positionObj.coords.latitude,
+//     longitude: positionObj.coords.longitude,
+//   };
 
-  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-  // eslint-disable-next-line no-undef
-  const addressObj = await getAddress(position);
-  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+//   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+//   // eslint-disable-next-line no-undef
+//   const addressObj = await getAddress(position);
+//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
-  // 3) Then we return an object with the data that we are interested in
-  return { position, address };
-}
+//   // 3) Then we return an object with the data that we are interested in
+//   return { position, address };
+// }
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  username: '',
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.action;
