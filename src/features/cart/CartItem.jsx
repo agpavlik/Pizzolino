a13ded9ef367bcaac55a9a5aec684bb0c7a7bb92
agpavlik <agpavlik@gmail.com>
2023-11-01 23:44:04 -0400

    Add delete Cart functionality

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import DeleteItem from './DeleteItem';
 
 function CartItem({ item }) {
-  const { name, quantity, totalPrice } = item;
+  const { name, quantity, totalPrice, pizzaId } = item;
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -12,7 +12,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
